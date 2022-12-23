3dbf6eb261a3d397f1f431009787d483a5e1d29a
BoyanDimov20 <boyandimov20@gmail.com>
2022-12-23 21:20:58 +0200

    Initial setup

---
+++ src/server/API/API/CommentPost.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API
+{
+    public class CommentPost
+    {
+        [Required]
+        public string Id { get; set; }
+
+        public string Content { get; set; }
+    }
+}
---
+++ src/server/API/API/Controllers/CommentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CommentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Post(CommentPost comment)
+        {
+            if (comment.Id is null || comment.Content is null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(comment.Id);
+        }
+    }
+}
